4265ed81b5d029db07615f6f62f5323fae874541
SongAlfio <Song.Alfio@iisgalvanimi.edu.it>
2022-11-07 10:45:50 +0000

    Aggiornamento

--- Angular-Flask/client-angular/src/app/app.module.ts
+++ Angular-Flask/client-angular/src/app/app.module.ts
@@ -2,13 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http'; //Importa Il modulo http
+import { FooComponent } from './foo/foo.component';
 
 @NgModule({
  declarations: [
-   AppComponent
+   AppComponent,
+   FooComponent
  ],
  imports: [
-   BrowserModule, HttpClientModule 
+   BrowserModule, HttpClientModule //Aggiungi il modulo http qui 
  ],
  providers: [],
  bootstrap: [AppComponent]
