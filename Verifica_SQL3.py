5ad8d9a4a7c4d93d0dc44f0ed96c97984260393c
SongAlfio <Song.Alfio@iisgalvanimi.edu.it>
2022-11-08 07:32:52 +0000

    Aggiornamento

--- Verifica_SQL3.py
+++ Verifica_SQL3.py
@@ -1,24 +1,26 @@
-#Realizzare un sito web che permetta di visualizzare l'elenco dei primi 10 clienti che hanno speso di più. 
-#Il maneger di BikeStore si collega alla rotta /bestCustomers e riceve l'elenco dei clienti. 
-#Cliccando poi sull'ID di uno dei clienti, si deve poter visualizzare l'elenco degli ordini effettuati. 
-#Utilizzare Bootstrap per l'interfaccia grafica.
-
-
-from flask import Flask, render_template, send_file, make_response, url_for, Response, request, redirect
+from flask import Flask, render_template, request, Response
 app = Flask(__name__)
+
+import io
 import pandas as pd 
+
 import pymssql
 
-@app.route('/bestCustomers', methods=['GET'])
+conn = pymssql.connect(server='213.140.22.237\SQLEXPRESS', user='song.alfio', password='xxx123##', database='song.alfio')
+
+@app.route("/bestCustomers", methods=["GET"])
 def bestCustomers():
-    conn = pymssql.connect(server='213.140.22.237\SQLEXPRESS', user='song.alfio', password='xxx123##', database='song.alfio') 
-    query = f"Select TOP 10 first_name, last_name from (select * from Sales.Customers inner join sales.orders on Sales.Customers.Customer_id = sales.orders.Customer_ID inner join Sales.order_Item on Sales.order_Item.Order_id = sales.orders.Order_id group by Customer_ID order by list_price desc) inner join sales.orders on Sales.Customers.Customer_id = sales.orders.Customer_ID inner join Sales.order_Item on Sales.order_Item.Order_id = sales.orders.Order_id"
-    df_Utente = pd.read_sql(query,conn)
-    return render_template("Verifica_SQL3/bestCustomers.html", nomiColonne = df_Utente.columns.values, dati = list(df_Utente.values.tolist()))
+    query = 'SELECT TOP 10 customers.customer_id, customers.first_name, customers.last_name ,SUM(list_price*quantity) as spesa_totale FROM sales.customers INNER JOIN sales.orders ON sales.customers.customer_id = sales.orders.customer_id INNER JOIN sales.order_items ON sales.order_items.order_id = sales.orders.order_id GROUP BY customers.customer_id, customers.first_name, customers.last_name ORDER BY SUM(list_price*quantity) DESC '
+    #INNER JOIN sales.oder_items ON sales.orders.order_id = sales.order_items.order_id
+    tabella = pd.read_sql(query,conn)
+    print(tabella.columns.values)
+    return render_template("Verifica_SQL3/bestCustomers.html", nomiColonne = tabella.columns.values, dati = tabella.values)
 
-
-
-
+@app.route("/totaleOrdini/<valore>")
+def totaleOrdini(valore):
+    query = f'SELECT * FROM sales.customers INNER JOIN sales.orders ON sales.customers.customer_id = sales.orders.customer_id WHERE customers.customer_id = {valore}'
+    tabella = pd.read_sql(query,conn)
+    return render_template("Verifica_SQL3/totaleOrdini.html", nomiColonne = tabella.columns.values, dati = tabella.values)
 
 if __name__ == '__main__':
   app.run(host='0.0.0.0', port=3245, debug=True)
