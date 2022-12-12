b6da11b2cf04404f225528fb4bd881c408c81e67
SongAlfio <Song.Alfio@iisgalvanimi.edu.it>
2022-12-12 08:49:43 +0000

    Aggiornamento

--- Progetto_Musica/Python/Progetto_Musica.py
+++ Progetto_Musica/Python/Progetto_Musica.py
@@ -1,44 +1,55 @@
-from flask import Flask, request, jsonify
+from flask import Flask, render_template, send_file, request, make_response, redirect, url_for, Response, redirect, jsonify
 import pandas as pd
 import pymssql as sql
 from flask_cors import CORS
-
 from os import getenv
 from dotenv import load_dotenv
 load_dotenv()
 
-db_server = getenv("DB_SERVER")
-db_user = getenv("DB_USER")
-db_password = getenv("DB_PASSWORD")
-db_name = getenv("DB_NAME")
 
 conn = sql.connect(server='213.140.22.237\SQLEXPRESS', user='song.alfio', password='xxx123##', database='song.alfio')
 
 app = Flask(__name__)
 CORS(app)
+#0. Home Page
+@app.route('/', methods=['GET'])
+def Home():
+  return render_template("Home.html")
 
 #1. Registrazione
 @app.route('/Sign_Up', methods=['GET'])
-def Sign_Up():
+def Sign_Up():    
+    return render_template("Sign_Up.html")
+
+@app.route('/Registrazione', methods=['GET'])
+def Registrazione():    
+
     Nome_utente = request.args['Nome_utente']
     Email = request.args['Email']
     Password = request.args['Password']
     Età = request.args['Età']
     Sesso = request.args['Sesso']
-    query=f"insert into Musica.Utente(Nick_Name, Email, Password, Età, Sesso) values('{Nome_utente}', '{Email}','{Password}','{Età}','{Sesso}')"    
-    df = pd.read_sql(query,conn)
+    df1 = pd.read_csv("static/csv/Musica.Utente.csv")
+
+    cursor = conn.cursor()
+    query = f"insert into Musica.Utente(Nick_Name, Email, Password, Età, Sesso) values('{Nome_utente}', '{Email}','{Password}','{Età}','{Sesso}')"
     
-    return render_template('Progetto_Musica/Home.html', url='/static/images/plot.png',url1='/static/images/plot1.png', url2='/static/images/plot2.png')
+    cursor.execute(query)
+    conn.commit()
+
+    return render_template("Home.html")
+    
 
 #2. Login
 @app.route('/Login', methods=['GET'])
 def Login():
     Nome_utente = request.args['Nome_utente']
-    Email = request.args['Email']
     Password = request.args['Password']
-    query= f"select * from Musica.Login where Nome_utente='{Nome_utente}' and Email='{Email}' and Password='{Password}'"
+    query= f"select * from Musica.Login where Nome_utente='{Nome_utente}' and Password='{Password}'"
+    df2 = pd.read_sql(query, conn, params={"data": f'Nome_utente', "data": f'Password'})
+    res = list(df2.fillna("NaN").to_dict("index").values())
+    return jsonify(res)
 
-    return render_template('Progetto_Musica/Home.html', url='/static/images/plot.png',url1='/static/images/plot1.png', url2='/static/images/plot2.png')
 
 #3. Modifica Informazioni Utente
 @app.route('/Modifica_Nome', methods=['GET'])
@@ -47,8 +58,9 @@ def Modifica_Nome():
     Email = request.args['Email']
     Password = request.args['Password']
     query=f"update Musica.Utente set Nome_utente = '{New_name}' where Email='{Email}' and Password='{Password}'"
+    df3 = pd.read_sql(query,conn)
 
-    return render_template('Progetto_Musica/Home.html', url='/static/images/plot.png',url1='/static/images/plot1.png', url2='/static/images/plot2.png')
+    return render_template('Progetto_Musica/Home.html', df3 = df3)
 
 @app.route('/Modifica_Email', methods=['GET'])
 def Modifica_Email():
@@ -56,16 +68,18 @@ def Modifica_Email():
     Email = request.args['Email']
     Password = request.args['Password']
     query=f"update Musica.Utente set Email= '{New_Email}' where where Email='{Email}' and Password='{Password}'"
+    df3_1 = pd.read_sql(query,conn)
 
-    return render_template('Progetto_Musica/Home.html', url='/static/images/plot.png',url1='/static/images/plot1.png', url2='/static/images/plot2.png')
+    return render_template('Progetto_Musica/Home.html', df3_1 = df3_1)
 
 @app.route('/Modifica_Pass', methods=['GET'])
 def Modifica_Pass():
     New_Pass = request.args['New_Pass']
     Old_Email = request.args['Email']
     query=f"update Musica.Utente set Password= '{New_Password}' where Email='{Old_Email}'"
+    df3_2 = pd.read_sql(query,conn)
 
-    return render_template('Progetto_Musica/Home.html', url='/static/images/plot.png',url1='/static/images/plot1.png', url2='/static/images/plot2.png')
+    return render_template('Progetto_Musica/Home.html', df3_2 = df3_2)
 
 #4. Visualizzazione info utente
 @app.route('/Info_Utente', methods=['GET'])
@@ -74,7 +88,9 @@ def Info_Utente():
     Email = request.args['Email']
     Password = request.args['Password']
     query=f"delete from Musica.Utente where Nome_utente='{Nome_Utente}' and Email='{Email}' and Password ='{Password}'"
-    return render_template('Progetto_Musica/Home.html', url='/static/images/plot.png',url1='/static/images/plot1.png', url2='/static/images/plot2.png')
+    df4 = pd.read_sql(query,conn)
+
+    return render_template('Progetto_Musica/Home.html', df4 = df4)
 
 #5. Rimozione account:
 @app.route('/Delete_Account', methods=['GET'])
@@ -82,8 +98,9 @@ def Delete_Account():
     New_Pass = request.args['New_Pass']
     Old_Email = request.args['Email']
     query=f"select Nome_Utente, Email, Password from Musica.Utente  where Email='{Email}' and Password='{Password}'"
+    df5 = pd.read_sql(query,conn)
 
-    return render_template('Progetto_Musica/Home.html', url='/static/images/plot.png',url1='/static/images/plot1.png', url2='/static/images/plot2.png')
+    return render_template('Progetto_Musica/Home.html', df5 = df5)
 
 
 
