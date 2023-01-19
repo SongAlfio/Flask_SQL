4874d0c52732042f745d6d7ab9cad2e5f3d916a1
SongAlfio <Song.Alfio@iisgalvanimi.edu.it>
2023-01-19 14:01:07 +0000

    Aggiornamento

--- Progetto_Musica/Angular/src/app/home/home.component.ts
+++ Progetto_Musica/Angular/src/app/home/home.component.ts
@@ -10,10 +10,12 @@ export class HomeComponent  implements OnInit{
 
   dataForHtml : any;
 
-  constructor(private http : HttpClient){}
+  constructor(
+    private http : HttpClient,
+    ){}
   
   ngOnInit(): void {
-      this.http.get("https://3245-songalfio-flasksql-yexagvfb5g8.ws-eu83.gitpod.io/Search2")
+      this.http.get("https://3245-songalfio-flasksql-j3ybzf1i4ho.ws-eu83.gitpod.io/Search2")
       .subscribe(this.getData)
   }
 
--- Progetto_Musica/Angular/src/app/musicista/musicista.component.ts
+++ Progetto_Musica/Angular/src/app/musicista/musicista.component.ts
@@ -13,7 +13,7 @@ export class MusicistaComponent  implements OnInit{
   constructor(private http : HttpClient){}
   
   ngOnInit(): void {
-      this.http.get("https://3245-songalfio-flasksql-yexagvfb5g8.ws-eu83.gitpod.io/Musicisti")
+      this.http.get("https://3245-songalfio-flasksql-j3ybzf1i4ho.ws-eu83.gitpod.io/Musicisti")
       .subscribe(this.getData)
   }
 
